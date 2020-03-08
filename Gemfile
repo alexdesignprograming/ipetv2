eda7c16ad3939a0ca988d44ff8515d0a06bc0346
Alex Silva <alexsilva10s@hotmail.com>
2020-03-08 20:39:15 -0300

    nice

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
-
+gem 'bootstrap-sass'
 #opcional image upload to Amazon S3
 gem 'carrierwave', "0.10.0"
 gem 'fog', '~> 1.37.0'
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,9 +1,5 @@
 class PublicController < ApplicationController
   def main
-  	if account_signed_in?
-  		redirect_to dashboard_path, flash:{ success: "Logado com sucesso. Bem Vindo ao Ipet!"} and return
-  	end
-
   	@properties = Property.latest
   end
 end
